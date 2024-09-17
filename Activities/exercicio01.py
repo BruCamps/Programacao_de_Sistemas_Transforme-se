3ac5fc4e886e8370d9ff19d4db6d97adeac7ea5c
Bruna Campos <86365466+BruCamps@users.noreply.github.com>
2024-09-17 19:25:30 -0300

    Create exercicio01.py

---
+++ Activities/exercicio01.py
@@ -0,0 +1,14 @@
+# Crie um programa que calcule o índice de massa corporal de uma pessoa.
+
+# Variáveis principais
+nome = input("\nInforme seu nome: ")
+altura = float(input("Informe sua altura: "))
+peso = float(input("Informe seu peso:  "))
+imc = peso / (altura * altura)
+
+# Variáveis de mensagem
+divisoria = f'{"-"*6} Resultado do IMC {"-"*6}'
+mensagem = f'\n{divisoria}\n\nOlá, {nome}!\n\nAltura: {altura}\nPeso: {peso}\nIMC: {imc:,.2f}\n'
+
+# Função de saída
+print(mensagem)
