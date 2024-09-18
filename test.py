f94fc677bff42685c023001777d0fa89779366e9
Bruna Campos <86365466+BruCamps@users.noreply.github.com>
2024-09-17 21:20:53 -0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+# idade = int(input("\nInforme a sua idade: "))
+
+# if idade >=18:
+#     print("Você é maior de idade")
+# else: 
+#     print("\nVocê é menor de idade")
+# print("Obrigado por usar o software\n")
+
+# nota = 91
+
+# if nota >= 90:
+#     print("Excelente")
+# elif nota >= 70:
+#     print("Bom")
+# elif nota >= 50:
+#     print("Suficiente")
+# else:
+#     print("Insuficiente")
+
