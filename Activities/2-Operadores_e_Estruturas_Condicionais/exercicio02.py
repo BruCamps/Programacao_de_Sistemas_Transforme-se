c7e5a2ceb1444c1a633686dd3474237a2ff5618e
Bruna Campos <86365466+BruCamps@users.noreply.github.com>
2024-09-19 19:50:33 -0300

    Update exercicio02.py

--- Activities/2-Operadores_e_Estruturas_Condicionais/exercicio02.py
+++ Activities/2-Operadores_e_Estruturas_Condicionais/exercicio02.py
@@ -7,9 +7,20 @@
 # Variável do Número
 num = float(input("\nDigite qualquer número: "))
 
-if num > 0:                                    # Condição para o número ser positivo (verifica se o número é maior que 0)
-    print(f"\nO número {num} é positivo\n") 
-elif num < 0:                                  # Condição para o número ser negativo (verifica se o número é menor que 0)
-    print(f"\nO número {num} é negativo\n")
+if num > 0:                                          # Condição para o número ser positivo (verifica se o número é maior que 0)
+    print(f"\nO número {num} é positivo\n\n\n\n") 
+elif num < 0:                                        # Condição para o número ser negativo (verifica se o número é menor que 0)
+    print(f"\nO número {num} é negativo\n\n\n\n")
 else:                                           
-    print(f"\nO número {num} é neutro\n")      # Resulado para o caso do número ser neutro (opcional)
+    print(f"\nO número {num} é neutro\n\n\n\n")      # Resultado para o caso do número ser neutro (opcional)
+
+# Obs.: A atividade pede APENAS if e else, mas resolvi criar uma elif para que tivesse a opção para número neutro.
+
+"""
+A resolução dessa atividade sem o número neutro ficaria dessa forma:
+
+if num > 0:                                       
+    print(f"\nO número {num} é positivo\n\n\n\n") 
+else:                                        
+    print(f"\nO número {num} é negativo\n\n\n\n")
+"""
