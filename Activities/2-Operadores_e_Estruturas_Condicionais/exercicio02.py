501e0ca3f4b71340048b0b25075c0fa1b1a8574d
Bruna Campos <86365466+BruCamps@users.noreply.github.com>
2024-09-18 23:41:18 -0300

    Update exercicio02.py

--- Activities/2-Operadores_e_Estruturas_Condicionais/exercicio02.py
+++ Activities/2-Operadores_e_Estruturas_Condicionais/exercicio02.py
@@ -7,9 +7,9 @@
 # Variável do Número
 num = float(input("\nDigite qualquer número: "))
 
-if num > 0:                                     # Condição para o número ser positivo (verifica se o número é maior que 0)
-    print(f"\nO número {num} é positivo\n")
-if num == 0:                                    # Condição para o número ser neutro (verifica se o número é igual a 0) (opcional)
-    print(f"\nO número {num} é neutro\n") 
+if num > 0:                                    # Condição para o número ser positivo (verifica se o número é maior que 0)
+    print(f"\nO número {num} é positivo\n") 
+elif num < 0:                                  # Condição para o número ser negativo (verifica se o número é menor que 0)
+    print(f"\nO número {num} é negativo\n")
 else:                                           
-    print(f"\nO número {num} é negativo\n")     # Resultado caso o número seja negativo
+    print(f"\nO número {num} é neutro\n")      # Resulado para o caso do número ser neutro (opcional)
