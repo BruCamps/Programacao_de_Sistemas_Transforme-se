0b0d87416bc3bfb814175a86565965cad91aa20c
Bruna Campos <86365466+BruCamps@users.noreply.github.com>
2024-09-25 19:12:49 -0300

    Update exercicio02.py

--- Activities/4-Estruturas_de_Repeticao/exercicio02.py
+++ Activities/4-Estruturas_de_Repeticao/exercicio02.py
@@ -2,34 +2,57 @@
 # a) A soma de idade das pessoas.
 # b) A média das idades lidas.
 
-nome = []
-idade = []
+# Variáveis de array
+arraynome = []
+arrayidade = []
+
+# Variáveis que armazena o nome da pessoa mais velha e mais nova
 maisvelha = ''
 maisnova = ''
-soma = 0
-media = 0
-maior = 0
-menor = 100
 
-for i in range(0, 5):
-    nome.append(str(input("Nome: ")))
-    idade.append(int(input("Idade: ")))
-    soma += idade[i]
+# Variáveis numéricas
+i = 0             
+soma = 0          
+media = 0         
+maior = 0         
+menor = 100       
 
-    if idade[i] > maior:
-        maior = idade[i]
-        maisvelha = nome[i]
-    if idade[i] < menor:
-        menor = idade[i]
-        maisnova = nome[i]
+# Estrutura de repetição
+while i < 5: 
 
-media = soma / len(idade) 
+    # Comandos de entrada para nome e idade
+    nome = input(f"\n{i+1}ª pessoa \nNome: ")
+    idade = int(input(f"Idade: "))
 
-print(f'''
+    # Armazenando os valores de nome e idade em seus respectivos arrays
+    arrayidade.append(idade)
+    arraynome.append(nome)
+
+    # Condicionais para idade maior e menor (opcional)
+    if arrayidade[i] > maior:
+        maior = arrayidade[i]
+        maisvelha = arraynome[i]
+    if arrayidade[i] < menor:
+        menor = arrayidade[i]
+        maisnova = arraynome[i]
+
+    # Adiciona +1 para o contador i
+    i += 1
+
+# Variáveis de cálculo
+soma = sum(arrayidade)
+media = soma / len(arrayidade) 
+
+
+print(
+# Mensagem com a soma e a média das idades
+f'''
 Soma das idades: {soma}
 Média das idades: {media:.1f}
+''',
+# Mensagem com os nomes das pessoas e suas idades (opcional)
+f'''
 A pessoa mais velha é {maisvelha} com {maior} {maior == 1 and 'ano' or 'anos'}
 A pessoa mais nova é {maisnova} com {menor} {menor == 1 and 'ano' or 'anos'}
 '''
 )
- 
