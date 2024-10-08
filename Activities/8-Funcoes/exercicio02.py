38ec3922a3e14f03b33f92b49a4cf1a0841e0ac2
Bruna Campos <86365466+BruCamps@users.noreply.github.com>
2024-10-08 14:10:31 -0300

    Create exercicio02.py

---
+++ Activities/8-Funcoes/exercicio02.py
@@ -0,0 +1,13 @@
+"""
+Exercício: Crie um programa em python que solicite um número qualquer. Nele, criem
+duas funções, uma para calcular o quadrado do número digitado e outra para
+imprimir o resultado.
+"""
+ 
+def calculo(num):
+    return num ** 2
+def resultado(num):
+    print(f'\n{num}² = {num} x {num} = {exp(num)}\n')
+ 
+numero = int(input("Digite um número: "))
+resultado(numero)
