1bfabbf4e0688224b69948afe27881ef1564f8b7
Bruna Campos <86365466+BruCamps@users.noreply.github.com>
2024-10-09 17:36:07 -0300

    Update exercicio01.py

--- Activities/9-POO/exercicio01.py
+++ Activities/9-POO/exercicio01.py
@@ -5,30 +5,14 @@ O programa deve dar a possibilidade do cliente exibir o saldo, depositar na
 conta e sacar da conta.
 """
 
-class Conta:
-    def __init__(self, nome, cpf, saldo=0):
-        self.nome = nome
-        self.cpf = cpf
-        self.saldo = saldo
-        self.deposito = 0
-        self.saque = 0
-        self.saldoFinal = 0
-
-    def msgFinal(self):
-        print(f'\nCliente: {self.nome}\nCPF: {self.cpf:0<11}\nSaldo: R${self.saldo:.2f}\n')
-
-    def exibeDeposito(self):
-        print(f'Depósito: R${self.deposito:.2f}\nSaldo: R${self.saldo:.2f}')
-
-    def exibeSaque(self):
-        print(f'Saque: R${self.saque:.2f}\nSaldo: R${self.saldo:.2f}')
+from conta import Conta
 
 try: 
     nome = input("Digite seu nome: ") or "Anônimo"
     cpf = int(input("Digite seu CPF: "))
     saldo = float(input("Digite seu saldo: "))
 
-    objeto = Conta(nome, cpf, saldo)
+    conta = Conta(nome, cpf, saldo)
     print(f"\nOlá! Bem-vindx, {nome}!\nO que deseja fazer hoje?")
 
     while True:
@@ -36,18 +20,18 @@ try:
         opcao = int(input(f"\nEscolha uma das opções:\n[1] Depositar\n[2] Sacar\n[3] Sair\n"))
 
         if opcao == 1:
-            objeto.deposito = float(input("Digite o valor do depósito: "))
-            objeto.saldo += objeto.deposito
-            objeto.exibeDeposito()
+            conta.deposito = float(input("Digite o valor do depósito: "))
+            conta.saldo += conta.deposito
+            conta.exibeDeposito()
 
         elif opcao == 2:
-            objeto.saque = float(input("Digite o valor do saque: "))
-            objeto.saldo -= objeto.saque
-            objeto.exibeSaque()
+            conta.saque = float(input("Digite o valor do saque: "))
+            conta.saldo -= conta.saque
+            conta.exibeSaque()
 
         else:
-            objeto.msgFinal()
+            conta.msgFinal()
             break
 
 except ValueError:
-    print("\n[ERRO: O valor informado não é um número! Tente novamente]\n")
+    print("\n\033[1;31m[ERRO: O valor informado não é um número! Tente novamente]\033[m\n")
