432d181b3c331c02c6fd0c95001e90aa41adbb8c
Bruna Campos <86365466+BruCamps@users.noreply.github.com>
2024-10-10 15:43:54 -0300

    Update exercicio01.py

--- Activities/9-POO/exercicio01.py
+++ Activities/9-POO/exercicio01.py
@@ -13,19 +13,18 @@ try:
     saldo = float(input("Digite seu saldo: "))
 
     conta = Conta(nome, cpf, saldo)
-    print(f"\nOlá! Bem-vindx, {nome}!\nO que deseja fazer hoje?")
+    print(f"\nOlá! Bem-vindx, \033[1;35m{nome}\033[m!\nO que deseja fazer hoje?")
 
     while True:
-
-        opcao = int(input(f"\nEscolha uma das opções:\n[1] Depositar\n[2] Sacar\n[3] Sair\n"))
+        opcao = int(input(f"\n[1] Depositar\n[2] Sacar\n[3] Sair\n\n\033[0;35mEscolha uma das opções:\033[m "))
 
         if opcao == 1:
-            conta.deposito = float(input("Digite o valor do depósito: "))
+            conta.deposito = float(input("\n\033[0;35mDigite o valor do depósito:\033[m "))
             conta.saldo += conta.deposito
             conta.exibeDeposito()
 
         elif opcao == 2:
-            conta.saque = float(input("Digite o valor do saque: "))
+            conta.saque = float(input("\n\033[0;35mDigite o valor do saque:\033[m "))
             conta.saldo -= conta.saque
             conta.exibeSaque()
 
